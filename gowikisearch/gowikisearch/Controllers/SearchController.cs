1f159fb63b3ae053f549ff7f50ce58828f738720
Kim Sunga <sungakim816@gmail.com>
2019-07-02 16:12:22 +0800

    Kahit papaano meron na akong query suggestions
    Trie data structure naman ang problemahin bukas

--- gowikisearch/gowikisearch/Controllers/SearchController.cs
+++ gowikisearch/gowikisearch/Controllers/SearchController.cs
@@ -5,6 +5,7 @@ using gowikisearch.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Korzh.EasyQuery.Linq;
+using System;
 
 namespace gowikisearch.Controllers
 {
@@ -26,7 +27,7 @@ namespace gowikisearch.Controllers
         [Route("Search/")]
         [Route("Search/{query}")]
         [Route("Search/{query}/{pageNumber:regex(^[1-9]{0,2}$)}")]
-        public ViewResult Index(string query, int? pageNumber)
+        public ActionResult Index(string query, int? pageNumber)
         {
             int pageSize = 6; // items per pages
             if (!pageNumber.HasValue)
@@ -46,19 +47,40 @@ namespace gowikisearch.Controllers
             return View(wikiPages.RetrievePages().ToPagedList((int)pageNumber, pageSize));
         }
 
+        [HttpGet]
+        [Route("Search/UpdatePagePopularity/{id:regex(^[1-9]$)}")]
+        [Route("Search/Update/Popularity/{id:regex(^[1-9]$)}")]
+        public ActionResult UpdatePagePopularity(int id)
+        {
+            try
+            {
+                WikipediaPageTitle pageTitleInstance = _context.WikipediaPageTitles.SingleOrDefault(p => p.Id == id);
+                pageTitleInstance.Popularity += 1;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+            return new EmptyResult();
+        }
+
         // GET: Search Autocomplete
         [HttpGet]
         [Route("Search/Autocomplete")]
         [Route("Search/Autocomplete/{query}")]
-        public ViewResult Autocomplete(string query)
+        public ActionResult Autocomplete(string query)
         {
-            query = "Hello";
+            if (query == null)
+            {
+                return new EmptyResult();
+            }
             query = query.ToLower();
-            int maxSuggestions = 10;
-            string sqlQuery = string.Format("SELECT TOP({0}) * " +
-                "FROM [dbo].[WikipediaPageTitle] " +
-                "WHERE CONTAINS(Title, '\"{1}*\"')", maxSuggestions, query
-                );
+            int maxSuggestions = 15;
+            string sqlQuery = string.Format("SELECT TOP({0}) * FROM (SELECT TOP(1000) * FROM [dbo].WikipediaPageTitle " +
+                "WHERE CONTAINS(Title, '\"{1}*\"')) as result WHERE result.Title LIKE '{1}%'" +
+                "ORDER BY result.Popularity DESC, result.Title;", maxSuggestions, query);
             ViewBag.DatabaseName = _context.Database.Connection;
             IEnumerable<WikipediaPageTitle> suggestions = _context.WikipediaPageTitles.SqlQuery(sqlQuery).AsEnumerable();
             return View(suggestions);
